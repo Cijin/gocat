ea872724dc89e862c3c561e70fef91dc0abb48d6
cijin <cijin@seagin.me>
2024-07-07 16:40:50 +0700

    run program on server and pipe output back

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 )
@@ -16,11 +18,13 @@ var (
 	listen bool
 	port   int
 	z      string
+	e      bool
 )
 
 func init() {
-	flag.BoolVar(&isUdp, "u", true, "Use UDP")
+	flag.BoolVar(&isUdp, "u", false, "Use UDP")
 	flag.BoolVar(&listen, "l", true, "Listen")
+	flag.BoolVar(&e, "e", false, "Turn a process into a server")
 	flag.IntVar(&port, "p", 8080, "Port to listen on")
 	flag.StringVar(&z, "z", "", "Connect to port without sending data")
 }
@@ -53,7 +57,7 @@ func main() {
 		return
 	}
 
-	listenTcp(port)
+	listenTcp(port, e)
 }
 
 func scanPorts(host, ports string) {
@@ -81,7 +85,7 @@ func scanPort(host string, p int64) {
 	conn.Close()
 }
 
-func listenTcp(port int) {
+func listenTcp(port int, e bool) {
 	fmt.Println("listening on port:", port)
 
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -97,12 +101,15 @@ func listenTcp(port int) {
 			log.Fatal("accept err:", err)
 		}
 
-		go func(c net.Conn) {
-			io.Copy(os.Stdout, c)
+		if e {
+			go execCommand(conn)
+		} else {
+			go func(c net.Conn) {
+				io.Copy(os.Stdout, c)
 
-			c.Close()
-		}(conn)
-
+				c.Close()
+			}(conn)
+		}
 	}
 }
 
@@ -125,10 +132,80 @@ func listenUdp(port int) {
 
 		log.Printf("recieved data %d bytes from client at addr: %s\n Data: %s", n, addr.String(), buf)
 
-		// write data back
 		_, err = packetConn.WriteTo([]byte(fmt.Sprintf("recieved %d bytes\n", n)), addr)
 		if err != nil {
 			log.Println("error sending data to client:", err)
 		}
+	}
+}
+
+func execCommand(conn net.Conn) {
+	fmt.Println("Executing shell...")
+	cmd := exec.Command("/bin/sh")
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Println("Error creating stdin pipe for cmd", err)
+		return
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println("Error creating stdout pipe for cmd", err)
+		return
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Println("Error creating stderr pipe for cmd", err)
+		return
+	}
+
+	defer func() {
+		err := stdin.Close()
+		if err != nil {
+			log.Println("Error closing stdin pipe:", err)
+		}
+
+		err = stdout.Close()
+		if err != nil {
+			log.Println("Error closing stdout pipe:", err)
+		}
+
+		err = stderr.Close()
+		if err != nil {
+			log.Println("Error closing stdout pipe:", err)
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal("Error starting command:", err)
+		return
+	}
+
+	go func() {
+		// what could go wrong :D
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			fmt.Fprintln(stdin, scanner.Text())
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Fprintln(conn, scanner.Text())
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			fmt.Fprintln(conn, scanner.Text())
+		}
+	}()
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
 	}
 }
